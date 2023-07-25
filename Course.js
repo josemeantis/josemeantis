3c8bece119e955ac88b5789eb5d0f789155df82a
josemeantis <83603740+josemeantis@users.noreply.github.com>
2023-07-25 00:06:45 -0500

    Add files via upload

---
+++ Course.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>;
+};
+
+const Part = ({ partName, exercises }) => {
+  return (
+    <p>
+      {partName} {exercises} exercises
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} partName={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ sum }) => {
+  return (
+    <p>
+      total of {sum} exercises
+    </p>
+  )
+}  
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <h4><Total sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/> </h4>
+    </div>
+  );
+};
+
+export default Course;
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+const course = [
+  // ...
+]
+
+ReactDOM.render(<App course={course} />, document.getElementById('root'))
